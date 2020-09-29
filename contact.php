5ff494956faf1ef130e6545174834adf66e25a3e
Laura Rodd <laura_rodd@hotmail.com>
2020-09-29 16:59:32 +0200

    added files

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
+
+$from = '';
+$sendTo = 'contact@laurarodd.com';
+$subject = 'New message from your portfolio';
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message');
+
+$okMessage = 'Thank you for contacting me. I will get back to you soon!.';
+$errorMessage = 'There is an error while sending message. Please try again later.';
+
+try {if (!empty($_POST)) {
+    $emailText = "You have a new message from your portfolio\n=====\n";
+    foreach ($_POST as $key => $value) {
+       if (isset($fields[$key])) {
+         $emailText .= "$fields[$key]: $value\n";
+       }
+    }
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+     'From: ' . $from,
+     'Reply-To: ' . $from,
+     'Return-Path: ' . $from,
+    );
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+    }
+}
+catch (\Exception $e) {
+  $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+ $encoded = json_encode($responseArray);
+ header('Content-Type: application/json');
+ echo $encoded;
+} else {
+echo $responseArray['message'];
+}
---
+++ js/script.js
@@ -0,0 +1,75 @@
+// Typing Effect Animation
+
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+const textArray = [ 'a Front-End Developer', 'a Problem Solver', 'Creative', 'a Web Developer', 'a Fast Learner' ];
+const typingDelay = 120;
+const erasingDelay = 100;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+// Typing animation
+
+function type() {
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		setTimeout(erase, newTextDelay);
+	}
+}
+
+// Delete text animation
+
+function erase() {
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + 1100);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	// On DOM Load initiate the effect
+	if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Toggle between dark and light mode
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('light-mode');
+});
+
+// Back to top button
+
+//Get the button:
+const topButton = document.getElementById('topBtn');
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+	scrollFunction();
+};
+function scrollFunction() {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		topButton.style.display = 'block';
+	} else {
+		topButton.style.display = 'none';
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topBtn() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
